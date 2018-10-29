983f52b3f60e6883b1c9dc7ee0dc934ff4504180
hanamichi <hanamichi@hanamichi.wiki>
2018-10-29 15:15:49 +0800

    update: update aria2.conf and on-complete.sh

--- script/on-complete.sh
+++ script/on-complete.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-if [ $2 -eq 1 ]; then
-	mv "$3" /data
-fi
-echo [$(date)] $2, $3, $1 "<br>" >> /data/_log.html
-
 
 DOWNLOAD=/data/downloading # no trailing slash!
 COMPLETE=/data/complete # no trailing slash!
